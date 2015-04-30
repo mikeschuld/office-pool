bf5eb53c1e33bc8f4147e1f87b4430daf6750f3c
Michael Schuld <mike.schuld@foundant.com>
2015-04-30 12:51:39 -0600

    Added ability to reuse Athletes on more than one Employee in the Athlete Pool

--- js/athlete.js
+++ js/athlete.js
@@ -10,55 +10,82 @@ $(function() {
         var athletes = $("#athletes").val().trim().split("\n");
         var employees = $("#employees").val().trim().split("\n");
         var per = $("#per").val();
+        var ePerA = $("#ePerA").val();
         
-        var max = athletes.length;
-        var used = new Array(max);
+        var athCount = athletes.length;
         var order = [];
 
-        // Fill the list in order
-        for(var i = 0; i < max; i++) {
+        // Fill the order list
+        for(var i = 0; i < athCount; i++) {
             order[i] = i;
         }
         
+        // Add placeholders to the page
+        addEmployeeSections(employees);
+        
         // Randomly swap cells 10000 times
         for(var s = 0; s < 10000; s++) {
-            var rand1 = Math.floor(Math.random() * max);
-            var rand2 = Math.floor(Math.random() * max);
+            var rand1 = Math.floor(Math.random() * athCount);
+            var rand2 = Math.floor(Math.random() * athCount);
             
             var temp = order[rand1];
             order[rand1] = order[rand2];
             order[rand2] = temp;
         }
         
-        addEmployeeSections(employees);
-        assignAthletes(order, athletes, per, max);
+        assignAthletes(order, athletes, per, ePerA, athCount);
     });
 });
 
 function addEmployeeSections(employees) {
     $(employees).each(function() {
         "use strict";
-
+        
         if(this.trim() == "") {
             return;
         }
-
+        
         var employee = templateLoad("athlete-employee.html");
         employee = templateReplace(employee, /\[employee\]/g, this);
         $(employee).appendTo(assigned);
     });
 }
 
-function assignAthletes(order, athletes, per, max) {
+function assignAthletes(order, athletes, per, ePerA, max) {   
     var slot = 0;
+    var used = new Array(order.length);
+    for (var u = 0; u < used.length; u++) {
+        used[u] = 0;
+    }
+    
     $(".employee").each(function() {
         var list = $("<ul>");
-        for(var a = 0; a < per && slot < max; a++) {
-            $("<li>" + athletes[order[slot++]] + "</li>").appendTo(list);
+        var assigned = 0;
+        
+        while(assigned < per && !allUsed(used, ePerA)) {
+            slot = slot % order.length;
+            
+            if(used[slot] < ePerA) {
+                $("<li>" + athletes[order[slot]] + "</li>").appendTo(list);
+                used[slot]++;
+                assigned++;
+                slot++;
+            }
         }
+        
         list.appendTo(this);
     });
 }
+
+function allUsed(used, max) {
+    for (var i = 0; i < used.length; i++) {
+        if (used[i] < max) {
+            return false;
+        }
+    }
+    
+    return true;
+};
 
 function templateLoad(url) {
     "use strict";
@@ -78,5 +105,5 @@ function templateLoad(url) {
 function templateReplace(template, find, replace) {
     "use strict";
 
-    return template.replace(find, replace);;
+    return template.replace(find, replace);
 }
